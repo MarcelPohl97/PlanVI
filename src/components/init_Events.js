9ecbf4f7d57d4358ebb372d84aa7f03ccfae391b
Marcel <45712008+MarcelPohl97@users.noreply.github.com>
2020-06-23 20:01:43 +0200

    modified:   dist/assets/bundle.js 	modified:   src/components/init_Events.js 	modified:   src/components/searchseries.js 	new file:   src/components/searchseriesEvent.js

--- src/components/init_Events.js
+++ src/components/init_Events.js
@@ -1,8 +1,10 @@
 import nav_Events from './nav_Events'
+import searchseries_Event from './searchseriesEvent';
 
 const init_Events = () => {
     document.addEventListener("DOMContentLoaded", () => {
         nav_Events();
+        searchseries_Event();
     })
 }
 

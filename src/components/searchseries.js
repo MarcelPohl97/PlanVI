018cdf13c40f41bd6b4fe7992da340c80370cfae
Marcel <45712008+MarcelPohl97@users.noreply.github.com>
2020-06-24 21:56:09 +0200

    modified:   src/components/searchseries.js 	modified:   src/components/searchseriesEvent.js

--- src/components/searchseries.js
+++ src/components/searchseries.js
@@ -4,14 +4,14 @@ const Searchseries = () => {
         <div class="card-panel white hoverable">
             <div class="row valign-wrapper">
                 <div class="col s12 m4 l4 xl2">
-                    <img src="https://serienstream.sx/public/img/cover/arrow-stream-cover-6gIKfH6QatAfQVpN2d0sUlsPZxCA8Hnz_220x330.jpg" alt="" class="responsive-img search-poster">
+                    <img src="https://serienstream.sx/public/img/cover/arrow-stream-cover-6gIKfH6QatAfQVpN2d0sUlsPZxCA8Hnz_220x330.jpg" alt="" id="search-poster" class="responsive-img">
                 </div>
                 <div class="col s12 m8 l8 xl10">
-                    <h6 class="black-text"> <span class="search-title">Arrow</span></h6>
-                    <p class="black-text"> <span class="search-plot">Die Serie „Arrow“ handelt vom früheren Playboy und Erbe eines großen Vermögens Oliver Queen (Stephen Amell). Nachdem er fünf Jahre auf sich alleine gestellt auf einer einsamen Insel überleben musste, kehrt er mit ungewöhnlichen neuen Fertigkeiten und einem geheimnisvollen Plan zurück in seine Heimatstadt Starling City und erklärt als Green Arrow dem Verbrechen den Kampf. In den Comics ist Green Arrow der klassischen Figur Robin Hood nachempfunden. In Maske und mit Pfeil und Bogen nimmt er die Gerechtigkeit als Superheld in die eigenen Hände, verwendet aber auch allerhand Trickpfeile. Obwohl sein Alter Ego Oliver Queen selbst Multimilliardär ist, hat er eine Vorliebe dafür, korrupten Firmen das Handwerk zu legen.</span></p>
-                    <p class="black-text">Produced: <span class="search-produced">Action, Adventure, Crime, Drama, Mystery, Sci-Fi</span></p>
-                    <p class="black-text">Genre: <span class="search-genre">Action, Adventure, Crime, Drama, Mystery, Sci-Fi</span></p>
-                    <p class="black-text">Year: <span class="search-year">2012-2020</span></p>
+                    <h6 class="black-text"> <span id="search-title">Arrow</span></h6>
+                    <p class="black-text"> <span id="search-plot">Die Serie „Arrow“ handelt vom früheren Playboy und Erbe eines großen Vermögens Oliver Queen (Stephen Amell). Nachdem er fünf Jahre auf sich alleine gestellt auf einer einsamen Insel überleben musste, kehrt er mit ungewöhnlichen neuen Fertigkeiten und einem geheimnisvollen Plan zurück in seine Heimatstadt Starling City und erklärt als Green Arrow dem Verbrechen den Kampf. In den Comics ist Green Arrow der klassischen Figur Robin Hood nachempfunden. In Maske und mit Pfeil und Bogen nimmt er die Gerechtigkeit als Superheld in die eigenen Hände, verwendet aber auch allerhand Trickpfeile. Obwohl sein Alter Ego Oliver Queen selbst Multimilliardär ist, hat er eine Vorliebe dafür, korrupten Firmen das Handwerk zu legen.</span></p>
+                    <p class="black-text">Produced: <span id="search-produced">Action, Adventure, Crime, Drama, Mystery, Sci-Fi</span></p>
+                    <p class="black-text">Genre: <span id="search-genre">Action, Adventure, Crime, Drama, Mystery, Sci-Fi</span></p>
+                    <p class="black-text">Year: <span id="search-year">2012-2020</span></p>
                     <a class="btn blue darken">Watch Online</a>
                     <div class="section"></div>
                 </div>
@@ -21,13 +21,14 @@ const Searchseries = () => {
             <div class="section"></div>
             <div class="row valign-wrapper">
                 <div class="input-field col s9 m10 l10 xl11">
-                    <input id="last_name" type="text" class="validate" id="search-input">
+                    <input type="text" class="validate">
                     <label for="last_name">Input Series</label>
                 </div>
                 <div class="col s3 m2 l2 xl1">
                     <a class="btn blue darken" id="searchseriesEvent">Search</a>
                 </div>
             </div>
+            <input type="text" id="search">
         </div>
     </div>
     `
--- src/components/searchseriesEvent.js
+++ src/components/searchseriesEvent.js
@@ -1,22 +1,23 @@
+const get_movie = (movie) => {
+    fetch(`http://www.omdbapi.com/?apikey=75046c65&t=${movie}`).then((response) => {
+              return response.json();
+          }).then(data => {
+              document.getElementById("search-poster").src = data["Poster"]
+              document.getElementById("search-title").innerHTML = data["Title"]
+              document.getElementById("search-plot").innerHTML = data["Plot"]
+          }).catch((err) => {
+              console.log("rejected", err);
+          })  
+      }
+
 
 
 
 const searchseries_Event = () => {
-    function get_movie(movie){
-        fetch(`http://www.omdbapi.com/?apikey=75046c65&t=${movie}`).then((response) => {
-                  return response.json();
-              }).then(data => {
-                  document.getElementByClassName("search-poster")[0].source = data["Poster"]
-                  document.getElementByClassName("search-title")[0].innerHTML = data["Title"]
-                  document.getElementByClassName("search-plot")[0].innerHTML = data["Plot"]
-              }).catch((err) => {
-                  console.log("rejected", err);
-              })  
-          }
+    const search_Input = document.getElementById("search");
     const searchseries_Event = document.getElementById("searchseriesEvent")
     searchseries_Event.addEventListener("click", () => {
-        const search_input = document.getElementById("search-input");
-        get_movie(search_input.value)
+        get_movie(search_Input.value);
     })
 }
 
